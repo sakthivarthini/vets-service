5f517ab85b85fb28f73542b3143cd6771642ab87
Pooja <149813908+sakthivarthini@users.noreply.github.com>
2024-07-18 12:09:52 +0530

    Update VetsServiceApplication.java

--- src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -20,6 +20,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.samples.petclinic.vets.system.VetsProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author Maciej Szarlinski
