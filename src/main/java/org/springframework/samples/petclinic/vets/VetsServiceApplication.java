550dae5ea90c772f2233e8ee10d5c75b53f8c699
Pooja <149813908+sakthivarthini@users.noreply.github.com>
2024-07-18 13:07:58 +0530

    Update VetsServiceApplication.java

--- src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -14,13 +14,11 @@
  * limitations under the License.
  */
 package org.springframework.samples.petclinic.vets;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.samples.petclinic.vets.system.VetsProperties;
-
 /**
  * @author Maciej Szarlinski
  */
@@ -28,8 +26,16 @@ import org.springframework.samples.petclinic.vets.system.VetsProperties;
 @SpringBootApplication
 @EnableConfigurationProperties(VetsProperties.class)
 public class VetsServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VetsServiceApplication.class, args);
 	}
 }
+
+
+
+
+
+
+
+
+
